2e83eb27e2b8290f499112178768214b41282963
duongpham17 <duongpham17@icloud.com>
2022-04-20 19:37:52 +0100

    download to desktop

--- src/constant/navbar/desktop/Desktop.js
+++ src/constant/navbar/desktop/Desktop.js
@@ -1,37 +1,23 @@
 import styles from './Desktop.module.scss';
 import React from 'react';
 
-import { connect } from 'react-redux';
-import { utilsDownloadOptions } from 'redux/actions/utilsActions';
-
 import { AiFillApple } from 'react-icons/ai';
 
 import Dropdown from 'components/dropdown';
-import useUrlDownload from 'hooks/useUrlDownload';
 
 import { mac } from "./app";
 
-const Desktop = (props) => {
-
-    const {utilsDownloadOptions} = props;
-
-    const {download} = useUrlDownload();
-
-    const onDownload = async (title, url) => {
-        utilsDownloadOptions("start", title);
-        await download(url, "meusic", "zip");
-        utilsDownloadOptions("end", title);
-    };
+const Desktop = () => {
 
     return (
         <div className={styles.container}>
             <Dropdown icon={<button className={styles.button}>Desktop</button>}>
                 <ul>
                     <li> 
-                        <button onClick={() => onDownload("MacOs installing", mac)}>
+                        <a href={mac} target="_blank" rel="noreferrer" download="name.zip">
                             <AiFillApple/> 
                             <span>Mac</span>
-                        </button>
+                        </a>
                     </li>
                 </ul>
             </Dropdown>
@@ -39,8 +25,4 @@ const Desktop = (props) => {
     )
 };
 
-const mapDispatchToProps = {
-    utilsDownloadOptions
-}
-
-export default connect(null, mapDispatchToProps)(Desktop)
+export default Desktop
