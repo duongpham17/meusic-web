f6938fd27b55234bb046306ff110553adfd84b6b
duongpham17 <duongpham17@icloud.com>
2022-05-25 15:41:40 +0100

    hash anchor tag

--- src/pages/savedPlaylist/songs/Alphabetical.js
+++ src/pages/savedPlaylist/songs/Alphabetical.js
@@ -1,12 +1,13 @@
 import styles from './Alphabetical.module.scss';
 import React, {useMemo} from 'react';
 import { generateid } from 'utils/generateid';
+import {HashLink} from 'react-router-hash-link';
 
 export const Alphabetical = (props) => {
 
   const {songsList} = props;
 
-  const memoFilteredSongList = useMemo(() => [...new Set(songsList.map(el => el.artist.slice(0, 1)))], [songsList]);
+  const memoFilteredSongList = useMemo(() => [...new Set(songsList.map(el => el.artist.slice(0, 1).toUpperCase()))], [songsList]);
 
   const sort = localStorage.getItem("savedPlaylist-sort") === "artist" || !localStorage.getItem("savedPlaylist-sort") === "artist";
 
@@ -15,7 +16,7 @@ export const Alphabetical = (props) => {
       <div className={styles.map}>
         {memoFilteredSongList.map(el => 
           <li key={generateid()}>
-            <a href={`#${el.toUpperCase()}`}>{el.toUpperCase()}</a>
+            <HashLink to={`/saved#${el}`}>{el.toUpperCase()}</HashLink>
           </li>
         )}
       </div>
